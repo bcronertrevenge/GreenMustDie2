b57c0373bdb9cdbed11d020482522b1c05bedfc6
dy-dev <dominique.yolin@free.fr>
2014-11-22 10:44:41 +0100

    Ajout de TheBoss et possibilité de rajouter des tourelles

---
+++ Source/GreenMustDie2/Tower.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "TheBoss.h"
+#include "GameFramework/Pawn.h"
+#include "Tower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GREENMUSTDIE2_API ATower : public APawn
+{
+	GENERATED_UCLASS_BODY()
+};
